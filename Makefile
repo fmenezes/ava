d462d02b1ea7d58d439cd03e4c8a8b0a9394ea8e
Filipe C Menezes <fcmenezes87@gmail.com>
2024-06-17 18:56:28 +0100

    format changes

---
+++ Makefile
@@ -0,0 +1,22 @@
+.DEFAULT_GOAL := help
+
+.PHONY: run
+run: ## Runs agent
+	poetry run start
+
+.PHONY: install
+install: ## Install dependencies
+	poetry install
+
+.PHONY: lint
+lint: ## Run linter
+	poetry run pylint ava
+
+.PHONY: format
+format: ## Format source code
+	poetry run isort ava
+	poetry run black ava
+
+.PHONY: help
+help:
+	@grep -E '^[a-zA-Z0-9_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
--- ava/main.py
+++ ava/main.py
@@ -1,5 +1,83 @@
-def _main():
-    print('hello world')
+import tempfile
+import wave
 
-if __name__ == '__main__':
+import numpy as np
+import pyaudio
+import whisper
+
+# Parameters
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+CHUNK = 1024
+SILENCE_THRESHOLD = 500  # Adjust this threshold based on your environment
+SILENCE_DURATION = 2  # Duration of silence in seconds to trigger stopping the recording
+
+model = whisper.load_model("large")
+
+
+def _is_silent(data_chunk, threshold=SILENCE_THRESHOLD):
+    """Returns 'True' if below the silence threshold."""
+    audio_data = np.frombuffer(data_chunk, dtype=np.int16)
+    return np.abs(audio_data).mean() < threshold
+
+
+def _record_audio(output_file: str, silence_duration: int = SILENCE_DURATION):
+    """Record audio from the microphone and save it to a WAV file, stopping on silence detection."""
+    audio = pyaudio.PyAudio()
+
+    stream = audio.open(
+        format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK
+    )
+
+    print("Recording...")
+
+    frames = []
+    silence_count = 0
+    silence_limit = int(RATE / CHUNK * silence_duration)
+
+    while True:
+        data = stream.read(CHUNK)
+        frames.append(data)
+
+        if _is_silent(data):
+            silence_count += 1
+        else:
+            silence_count = 0
+
+        if silence_count > silence_limit:
+            break
+
+    print("Finished recording due to silence detection.")
+
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
+
+    wf = wave.open(output_file, "wb")
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(audio.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b"".join(frames))
+    wf.close()
+
+
+def _get_transcribe(audio: str, language: str = "en"):
+    return model.transcribe(audio=audio, language=language)
+
+
+def _main():
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        audio_path = f"{tmpdirname}/audio.wav"
+        print(audio_path)
+        _record_audio(output_file=audio_path, silence_duration=SILENCE_DURATION)
+        result = _get_transcribe(audio=audio_path)
+        if result:
+            print("-" * 50)
+            print(result.get("text", "No transcription text found."))
+        else:
+            print("Transcription failed.")
+
+
+if __name__ == "__main__":
     _main()
