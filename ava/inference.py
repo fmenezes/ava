4bceab97826e4a55a2127b61e7c237fdc6ca7f8a
Filipe C Menezes <fcmenezes87@gmail.com>
2024-06-20 18:39:38 +0100

    add langchain

--- ava/inference.py
+++ ava/inference.py
@@ -1,2 +1,22 @@
+from langchain_community.chat_models.ollama import ChatOllama
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+
+prompt = ChatPromptTemplate.from_template(
+    """You're A.V.A. (Artificial Virtual Assistant), help with this query:
+
+{input}"""
+)
+
+output_parser = StrOutputParser()
+model = ChatOllama(model="llama2:13b")
+chain = (
+    {"input": RunnablePassthrough()}
+    | prompt
+    | model
+    | output_parser
+)
+
 def infere(prompt: str) -> str:
-    return "hello: " + prompt
+    return chain.invoke(prompt)
